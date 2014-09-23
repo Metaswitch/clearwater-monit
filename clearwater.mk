7d160d4d5e7fc22a8b06e70b088239b9b0ffdc31
Rob Day <Robert.Day@metaswitch.com>
2014-09-23 16:49:25 +0100

    Hook into existing Clearwater build infrastructure
    Conflicts:
    	debian/changelog

---
+++ clearwater.mk
@@ -0,0 +1,15 @@
+DEB_COMPONENT := monit
+DEB_MAJOR_VERSION := 5.9${DEB_VERSION_QUALIFIER}
+DEB_NAMES := monit
+
+include build-infra/cw-deb.mk
+
+.PHONY: deb-build-monit
+deb-build-monit:
+	echo "${DEB_COMPONENT} (${DEB_VERSION}) unstable; urgency=low" >debian/changelog
+	echo " * build from revision $$(git rev-parse HEAD)" >>debian/changelog;\
+	echo " -- $(CW_SIGNER_REAL) <$(CW_SIGNER)> $$(date -R)" >>debian/changelog
+	debuild --no-lintian -b -uc -us
+
+.PHONY: deb
+deb: deb-build-monit deb-move
