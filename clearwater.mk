9103d3e551f2132412d0800592cedf6a54a7e1fe
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-11-26 19:24:08 +0000

    Update mk file

--- clearwater.mk
+++ clearwater.mk
@@ -1,14 +1,14 @@
 DEB_COMPONENT := clearwater-monit
-DEB_MAJOR_VERSION := 5.9${DEB_VERSION_QUALIFIER}
+DEB_MAJOR_VERSION := 5.10${DEB_VERSION_QUALIFIER}
 DEB_NAMES := clearwater-monit
 
 include build-infra/cw-deb.mk
 
 .PHONY: deb-build-monit
 deb-build-monit:
-	echo "${DEB_COMPONENT} (${DEB_VERSION}) unstable; urgency=low" >debian/changelog
-	echo " * build from revision $$(git rev-parse HEAD)" >>debian/changelog;\
-	echo " -- $(CW_SIGNER_REAL) <$(CW_SIGNER)> $$(date -R)" >>debian/changelog
+	echo -e "${DEB_COMPONENT} (${DEB_VERSION}) unstable; urgency=low\n" >debian/changelog
+	echo -e "  * build from revision $$(git rev-parse HEAD)\n" >>debian/changelog
+	echo -e " -- $(CW_SIGNER_REAL) <$(CW_SIGNER)>  $$(date -R)\n" >>debian/changelog
 	debuild --no-lintian -b -uc -us
 
 .PHONY: deb
