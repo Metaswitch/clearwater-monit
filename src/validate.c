1a874499e1638b98429ebbe8eda0ac13f7828321
tildeslash <info@tildeslash.com>
2015-10-13 21:15:54 +0200

    fix Issue #244 : do not monitor if required service has errors

--- src/validate.c
+++ src/validate.c
@@ -981,8 +981,13 @@ static boolean_t _checkSkip(Service_T s) {
         // Skip if parent is not initialized
         for (Dependant_T d = s->dependantlist; d; d = d->next ) {
                 Service_T parent = Util_getService(d->dependant);
-                if (parent->monitor != Monitor_Yes)
+                if (parent->monitor != Monitor_Yes) {
+                        DEBUG("'%s' test skipped as required service '%s' is %s\n", s->name, parent->name, parent->monitor == Monitor_Init ? "initializing" : "not monitored");
                         return true;
+                } else if (parent->error) {
+                        DEBUG("'%s' test skipped as required service '%s' has errors\n", s->name, parent->name);
+                        return true;
+                }
         }
         return false;
 }
