2aaf416259bbb9405bd34f621c664b09947b070c
Tildeslash <git@tildeslash.com>
2015-10-22 12:32:15 +0200

    Issue #266: Only guard task_for_pid, allow sysctl and *BSD api's to collect data

--- src/process/sysdep_DARWIN.c
+++ src/process/sysdep_DARWIN.c
@@ -200,14 +200,6 @@ int initprocesstree_sysdep(ProcessTree_T **reference) {
         size_t             size;
         int                mib[4];
 
-        /* Issue #266: As of OS X 10.11 a new System Integrity Protection policy
-         (SIP) is in use which deny usage of task_for_pid, i.e. we cannot get 
-         process info and to continue here would be useless. SIP is enabled by
-         default on 10.11. If we are running as non-root 'isSipEnabled' is also
-         set to true, as we are not allowed to call task_for_pid then neither */
-        if (isSipEnabled)
-                return 0;
-
         mib[0] = CTL_KERN;
         mib[1] = KERN_PROC;
         mib[2] = KERN_PROC_ALL;
@@ -286,6 +278,43 @@ int initprocesstree_sysdep(ProcessTree_T **reference) {
                         pt[i].zombie = true;
                 pt[i].time = get_float_time();
 
+                /* Issue #266: As of OS X 10.11 a new System Integrity Protection policy
+                 (SIP) is in use which deny usage of task_for_pid, i.e. we cannot get
+                 process info and to continue here would be useless. SIP is enabled by
+                 default on 10.11. If we are running as non-root 'isSipEnabled' is also
+                 set to true, as we are not allowed to call task_for_pid then neither */
+                if (! isSipEnabled) {
+                        if (task_for_pid(mytask, pt[i].pid, &task) == KERN_SUCCESS) {
+                                mach_msg_type_number_t   count;
+                                task_basic_info_data_t   taskinfo;
+                                thread_array_t           threadtable;
+                                unsigned int             threadtable_size;
+                                thread_basic_info_t      threadinfo;
+                                thread_basic_info_data_t threadinfo_data;
+
+                                count = TASK_BASIC_INFO_COUNT;
+                                if (task_info(task, TASK_BASIC_INFO, (task_info_t)&taskinfo, &count) == KERN_SUCCESS) {
+                                        pt[i].mem_kbyte   = (unsigned long)(taskinfo.resident_size / 1024);
+                                        pt[i].cputime     = (long)((taskinfo.user_time.seconds + taskinfo.system_time.seconds) * 10 + (taskinfo.user_time.microseconds + taskinfo.system_time.microseconds) / 100000);
+                                        pt[i].cpu_percent = 0;
+                                }
+                                if (task_threads(task, &threadtable, &threadtable_size) == KERN_SUCCESS) {
+                                        threadinfo = &threadinfo_data;
+                                        for (int j = 0; j < threadtable_size; j++) {
+                                                count = THREAD_BASIC_INFO_COUNT;
+                                                if (thread_info(threadtable[j], THREAD_BASIC_INFO, (thread_info_t)threadinfo, &count) == KERN_SUCCESS) {
+                                                        if ((threadinfo->flags & TH_FLAGS_IDLE) == 0) {
+                                                                pt[i].cputime += (long)((threadinfo->user_time.seconds + threadinfo->system_time.seconds) * 10 + (threadinfo->user_time.microseconds + threadinfo->system_time.microseconds) / 100000);
+                                                                pt[i].cpu_percent = 0;
+                                                        }
+                                                }
+                                                mach_port_deallocate(mytask, threadtable[j]);
+                                        }
+                                        vm_deallocate(mytask, (vm_address_t)threadtable,threadtable_size * sizeof(thread_act_t));
+                                }
+                                mach_port_deallocate(mytask, task);
+                        }
+                }
                 if (task_for_pid(mytask, pt[i].pid, &task) == KERN_SUCCESS) {
                         mach_msg_type_number_t   count;
                         task_basic_info_data_t   taskinfo;
@@ -319,9 +348,9 @@ int initprocesstree_sysdep(ProcessTree_T **reference) {
         }
         FREE(args);
         FREE(pinfo);
-
+        
         *reference = pt;
-
+        
         return (int)treesize;
 }
 
